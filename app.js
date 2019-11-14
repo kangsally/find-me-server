6cb9aee57133194317077ec339ddaa222df3f9a2
kangsuroro <kangsuroro2907@gmail.com>
2019-11-14 21:55:02 +0900

    revise cors url

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 
 app.use(cors({
   credentials: true,
-  origin: 'https://awesome-kirch-9a9026.netlify.com'
+  origin: 'https://www.find-me.website'
 }));
 
 app.use(bodyParser.json());
